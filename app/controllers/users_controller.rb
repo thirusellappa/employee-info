8285e3c0af55612573b647d5ab64f6f319402208
Thirumalainayagi <thirumalainayagis@gmail.com>
2014-05-18 01:54:36 +0530

    find_by_id changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,25 +12,26 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save
-      @user.calculate_pay
-      redirect_to @user
-    else
-      p @user.errors
-      render "new"
-    end
+		@us
+    #~ if @user.save
+      #~ @user.calculate_pay
+      #~ redirect_to @user
+    #~ else
+      #~ p @user.errors
+      #~ render "new"
+    #~ end
   end
   
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find_by_id(params[:id])
   end
   
   def edit
-    @user = User.find_by(params[:id])
+    @user = User.find_by_id(params[:id])
   end
   
   def update
-    @user = User.find_by(params[:id])
+    @user = User.find_by_id(params[:id])
     if @user.update(user_params)
       @user.calculate_pay
       redirect_to @user
@@ -40,7 +41,7 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    @user = User.find_by(params[:id])
+    @user = User.find_by_id(params[:id])
     @user.destroy
     respond_to do |format|
       format.html {redirect_to root_path}
